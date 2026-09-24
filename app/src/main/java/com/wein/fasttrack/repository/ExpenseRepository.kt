ef2e2c308daf395e289819da0d21fa69759c4f78
Rifa Zam Zami <rifazamzami13@gmail.com>
2026-09-24 15:43:29 +0700

    feat: implement ExpenseRepository for managing expenses and tags

--- app/src/main/java/com/wein/fasttrack/repository/ExpenseRepository.kt
+++ app/src/main/java/com/wein/fasttrack/repository/ExpenseRepository.kt
@@ -23,6 +23,7 @@ class ExpenseRepository(
         }
     }
 
+    private fun getStartOfDay(): Long {
         val calendar = Calendar.getInstance()
         calendar.set(Calendar.HOUR_OF_DAY, 0)
         calendar.set(Calendar.MINUTE, 0)
